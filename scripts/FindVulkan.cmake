14541a192856ff4de0eccdc70351f74e7f2a6e54
NikitaWeW <nikita2510mart@gmail.com>
2024-09-22 18:37:03 +0300

    moved vulkan loader and vulkan headers directly to dependencies

--- scripts/FindVulkan.cmake
+++ scripts/FindVulkan.cmake
@@ -7,12 +7,12 @@
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 if(NOT VULKAN_INCLUDE_DIRS)
-    set(VULKAN_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/vulkan/Vulkan-Headers/include/ CACHE STRING "path to vulkan include dirs")
+    set(VULKAN_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/Vulkan-Headers/include/ CACHE STRING "path to vulkan include dirs")
 endif()
 
 if(NOT VULKAN_LIBRARIES)
     execute_process( # manualy configure and install vulkan headers (i have no idea)
-        COMMAND ${CMAKE_COMMAND} -S ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/vulkan/Vulkan-Headers -B ${CMAKE_CURRENT_BINARY_DIR}/Vulkan-Headers 
+        COMMAND ${CMAKE_COMMAND} -S ${CMAKE_CURRENT_SOURCE_DIR}/dependencies/Vulkan-Headers -B ${CMAKE_CURRENT_BINARY_DIR}/Vulkan-Headers 
             -G ${CMAKE_GENERATOR}
             -D CMAKE_MAKE_PROGRAM=${CMAKE_MAKE_PROGRAM}
         RESULT_VARIABLE RESULT
@@ -33,7 +33,7 @@ if(NOT VULKAN_LIBRARIES)
     endif()
 
     set(VULKAN_HEADERS_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/Vulkan-Headers)
-    add_subdirectory(dependencies/vulkan/Vulkan-Loader ${CMAKE_CURRENT_BINARY_DIR}/Vulkan-Loader)
+    add_subdirectory(dependencies/Vulkan-Loader ${CMAKE_CURRENT_BINARY_DIR}/Vulkan-Loader)
     set(VULKAN_LIBRARIES vulkan CACHE STRING "path to vulkan libraries")
     unset(RESULT)
 endif()
